65201f804217699fe3f0a05aa8f98cdf27926323
Francisco Huergo <franhuergo@gmail.com>
2018-07-28 18:40:52 -0400

    cart frontend complete (minus aesthetics)

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+const stripe = require("stripe")("sk_test_TwTTlid3GeOG6YPydOjARw4I");
 module.exports = router
 
 router.get('/', async (req, res, next) => {
@@ -75,3 +76,19 @@ router.get('/:userOrGuest/cart', async (req, res, next) => {
     next(err);
   }
 });
+
+router.post("/charge", async (req, res) => {
+  try {
+    let {status} = await stripe.charges.create({
+      amount: 2000,
+      currency: "usd",
+      description: "An example charge",
+      source: req.body
+    });
+
+    res.json({status});
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
