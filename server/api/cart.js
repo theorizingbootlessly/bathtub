aa56b2111c4d299dbb076241f5212eadbf835257
Wayne Hong <woo.young.hong@hotmail.com>
2018-07-29 13:22:05 -0400

    switching branch

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,12 +1,11 @@
 const router = require('express').Router()
 const {Cart} = require('../db/models')
 
-
 router.post('/:userOrGuest/cart', async (req, res, next) => {
   try {
     const product = {
       productId: req.body.id
-     };
+    }
 
     const cart = await Cart.findAll({
       where: {
@@ -15,30 +14,29 @@ router.post('/:userOrGuest/cart', async (req, res, next) => {
     })
 
     if (!cart || !product) {
-      res.sendStatus(404);
-    }
-    // if (user === 'guest') {
+      res.sendStatus(404)
+    } else {
+      // if (user === 'guest') {
       // if (!req.session.cart) {
       //   req.session.cart = [product];
       // } else {
       //   req.session.cart.push(product);
       //   res.status(201).send(product);
       // }
-    // } 
-    
-    else {
-    await Cart.findOrCreate({
-      where: {
-        productId: req.body.id,
-        quantity: req.body.quantity,
-        userId: req.params.userOrGuest
-      }
-    });
-    res.status(201).send(product);
-  } 
-} catch (err) {
-    next(err);
+      // }
+
+      await Cart.findOrCreate({
+        where: {
+          productId: req.body.id,
+          quantity: req.body.quantity,
+          userId: req.params.userOrGuest
+        }
+      })
+      res.status(201).send(product)
+    }
+  } catch (err) {
+    next(err)
   }
-});
+})
 
 module.exports = router
