1aa8aef30681ecc36bd61e00f333cab7ddae8a80
Wayne Hong <woo.young.hong@hotmail.com>
2018-07-30 12:09:15 -0400

    set save unintialized to true

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
-const stripe = require("stripe")("sk_test_TwTTlid3GeOG6YPydOjARw4I");
+const stripe = require('stripe')('sk_test_TwTTlid3GeOG6YPydOjARw4I')
 module.exports = app
 
 // This is a global Mocha hook, used for resource cleanup.
@@ -60,7 +60,7 @@ const createApp = () => {
       secret: process.env.SESSION_SECRET || 'we sell ducks',
       store: sessionStore,
       resave: false,
-      saveUninitialized: false
+      saveUninitialized: true
     })
   )
   app.use(passport.initialize())
