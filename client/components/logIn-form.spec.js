186d19f1815d9cb1abc9692a5eb0a132d37e8008
Jordan Langsam <jlangsam89@gmail.com>
2018-07-31 20:07:29 -0400

    fixed some tests and added a few login tests

--- client/components/logIn-form.spec.js
+++ client/components/logIn-form.spec.js
@@ -5,12 +5,25 @@ const expect = chai.expect
 //Enzyme test materials
 import {shallow} from 'enzyme'
 import React from 'react'
-import LogIn from './logIn-form'
+import {LogIn} from './logIn-form'
 
 
-describe('<LogIn />', () => {
-  it('renders a form input for email address', () => {
-     const wrapper = shallow(<LogIn />)
-     expect(wrapper).to.be.class
+describe('Login', () => {
+
+  let login
+
+  beforeEach(() => {
+    login = shallow(<LogIn />)
+  })
+
+  it('should have an email state', () => {
+
+     const email = login.state().email
+     expect(email).to.deep.equal('')
+  })
+
+  it('should have a password state', () => {
+    const password = login.state().password
+    expect(password).to.deep.equal('')
   })
 })
